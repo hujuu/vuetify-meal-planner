9d1e7ac9db05313aa7827f27387f58ac3e520815
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-17 18:18:34 +0900

    Create MealPlanner.vue

---
+++ src/components/MealPlanner.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import CalendarDays from './CalendarDays.vue';
+</script>
+
+<template>
+  <calendar-days :date="new Date()" :days="7" />
+</template>
+
+<style scoped>
+
+</style>
