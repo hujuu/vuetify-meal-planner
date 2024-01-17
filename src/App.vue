9dede7876eb57aed75f3066e17061f37b92e4594
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-17 18:18:12 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <v-main>
       <router-view />
     </v-main>
+    <v-footer>
+      <span class="text-light-green">My Meal Planner</span>&nbsp;- &copy; {{ new Date().getFullYear() }}
+    </v-footer>
   </v-app>
 </template>
 
