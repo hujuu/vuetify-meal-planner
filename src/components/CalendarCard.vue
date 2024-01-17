fc47eadd70554519b65340a95ee6c3ee85ec6c23
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-17 18:22:32 +0900

    Create CalendarCard.vue

---
+++ src/components/CalendarCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useFormatDate } from "@/composables/formatters";
+interface Card {
+  date: Date;
+}
+const props = defineProps<{
+  card: Card;
+}>();
+</script>
+
+<template>
+  <v-sheet class="d-flex justify-space-between">
+    <v-sheet class="ma-2 pa-2">
+      <h2 class="text-h2">{{ useFormatDate(card.date) }}</h2>
+    </v-sheet>
+  </v-sheet>
+</template>
+
+<style scoped>
+
+</style>
